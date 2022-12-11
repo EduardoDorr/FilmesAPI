8fc8405d9a9ea3892fc6457e4aef344ab48ff64d
Eduardo da Silva Dörr <edudorr@hotmail.com>
2022-12-10 21:20:08 -0300

    Aula 2

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,14 +8,29 @@ namespace FilmesAPI.Controllers
   public class FilmeController : ControllerBase
   {
     private static List<Filme> _filmes = new List<Filme>();
+    private static int _id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
+      filme.Id = _id++;
       _filmes.Add(filme);
-      Console.WriteLine(filme.Titulo);
-      Console.WriteLine(filme.Genero);
-      Console.WriteLine(filme.Duracao);
+      
+      return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 50)
+    {
+      return _filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+      var filme = _filmes.FirstOrDefault(f => f.Id == id);
+
+      return filme == null ? NotFound() : Ok(filme);
     }
   }
 }
